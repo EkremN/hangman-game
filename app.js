2c1069ac94478985278d559405f8995a5e9bce87
Ekrem Nurovic <ekrem.nurovic@gmail.com>
2020-05-16 22:05:09 +0200

    HTTP requests

--- app.js
+++ app.js
@@ -15,3 +15,45 @@ window.addEventListener("keypress", (e) => {
     elementH2.textContent = game1.StatusMessage
     elemtnP.textContent =game1.pazzleGame
 })
+
+//Making HTTP request
+
+// const request = new XMLHttpRequest()
+
+// request.addEventListener('readystatechange', (e) => {
+//     if(e.target.readyState === 4 && e.target.status === 200) {
+//         const data = JSON.parse(e.target.responseText)
+//         console.log(data)
+//     } else if (e.target.readyState === 4) {
+//         console.log("Error happened")
+//     }
+// })
+
+// request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
+// request.send()
+
+const countryCode = "BA"
+const request2 = new XMLHttpRequest()
+
+request2.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText)
+        
+        // second way of doing it
+        // const country = data.find((country) => country.alpha2Code === countryCode)
+        // console.log(country.name)
+        
+        for(let i = 0; i < data.length; i++) {
+            if (data[i].alpha2Code === countryCode) {
+                console.log(data[i].name)
+            }
+        }
+    } else {
+        if(e.target.readyState === 4) {
+            console.log("You have an error!!!")
+        }
+    }
+})
+
+request2.open('GET', 'http://restcountries.eu/rest/v2/all')
+request2.send()
